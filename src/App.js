de8e3bf98e9fe571c41fbe5d3c6be8ce18419f89
Gilberto Olivas <gilbertoolivas@Apex-Standard-PN2GJG69J1.local>
2025-10-02 17:42:26 -0600

    create methods to add and list my tasks

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-
-function InnerComponent() {
-  let [counter, setCounter] = useState(0)
-
-  const handleUpdateCounter = () => {
-    const nextState = counter + 1;
-    setCounter(nextState)
-    console.log(nextState)
-  }
-
-   const handleDecrementCounter = () => {
-    const nextState = counter - 1;
-    setCounter(nextState)
-    console.log(nextState)
-  }
-
-   const handleResetCounter = () => {
-    setCounter(0)
-  }
-
-  return (
-    <div>
-      <p>
-        Counter {counter}
-      </p>
-      <button onClick={handleUpdateCounter}>Increment</button>
-    <button onClick={handleDecrementCounter}>Decrement</button>
-    <button onClick={handleResetCounter}>Reset</button>
-    </div>
-  )
-}
+import { useEffect, useState } from "react";
+import { getAllTasks } from "./data";
 
 function App() {
+  let [allTasks, setAllTasks] = useState();
 
-  // hooks
-  // useState
-
+  useEffect(() => {
+    setAllTasks(getAllTasks());
+  }, [])
 
   return (
     <div className="App">
-       <InnerComponent />
+      <p>{JSON.stringify(allTasks)}</p>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,74 +1,139 @@
-const db = {}
-// id
-// name
-// description
-// acceptanceCritera
-// discussion
-// assignedTo
-// estimatedTime
-// burnedTime
-// pendingTime
-// status
-// activityType
-// relatedTask
+const db = {
+  uroqwurioqwue: {
+    id: "uroqwurioqwue",
+    name: "testing",
+    description: "my super very very very long description",
+  },
+};
 
 class UndefinedError extends Error {
   constructor(message) {
-    this.message = this._messge;
-    this.name = "UndefinedNewTaskError";
     super();
+    this.message = message;
+    this.name = "UndefinedNewTaskError";
   }
 }
 
 class NonValidTypeError extends Error {
   constructor(message) {
-    this.message = this._messge;
-    this.name = "NonValidTypeError";
     super();
+    this.message = message;
+    this.name = "NonValidTypeError";
   }
 }
 
 class NonValidValueError extends Error {
   constructor(message) {
-    this.message = this._messge;
-    this.name = "NonValidValueError";
     super();
+    this.message = message;
+    this.name = "NonValidValueError";
   }
 }
 
-/**
- *
- * @param {{
- * id: string;
- * name: string;
- * }} newTask
- * @returns
- */
+const TaskStatus = Object.freeze({
+  new: "NEW",
+  in_process: "IN PROGRESS",
+  review: "REVIEW",
+  finished: "FINISHED",
+});
+
 const createTasks = (newTask) => {
   if (newTask === undefined) {
-    throw new UndefinedError("New task is invalid, please provide a valid object.");
+    throw new UndefinedError(
+      "New task is invalid, please provide a valid object.",
+    );
   }
 
-  if (!newTask.hasOwnProperty("id") &&
-  !newTask.hasOwnProperty("name") &&
-  !newTask.hasOwnProperty("description")
-  ) {
-    throw new UndefinedError("New task is invalid, please provide a task with required properties.");
+  let fieldsWithError = ["id", "name", "description"].filter(
+    (requiredFieldName) => !newTask.hasOwnProperty(requiredFieldName),
+  );
+  if (fieldsWithError.length > 0) {
+    throw new UndefinedError(
+      `New task is invalid, please provide a ${fieldsWithError.toString()}.`,
+    );
   }
 
-  if (typeof newTask.id !== "string") {
-    throw new NonValidTypeError("New task is invalid, please provide a task with a correct id type.");
+  fieldsWithError = [newTask.id, newTask.name, newTask.description].filter(
+    (requiredFieldValue) => typeof requiredFieldValue !== "string",
+  );
+  if (fieldsWithError.length > 0) {
+    throw new NonValidTypeError(
+      `New task is invalid, please provide a ${fieldsWithError.toString()}.`,
+    );
   }
 
-  if (newTask.id.length === 10) {
-    throw new NonValidValueError("New task is invalid, please provide a task with a correct id type.");
+  fieldsWithError = [
+    {
+      id: {
+        length: 10,
+      },
+    },
+    {
+      name: {
+        minLength: 5,
+        maxLength: 12,
+      },
+    },
+    {
+      description: {
+        minLength: 5,
+        maxLength: 120,
+      },
+    },
+  ].filter((requiredField) => {
+    const { id: newId, name: newName, description: newDescription } = newTask;
+    const {
+      id: ruleForId,
+      name: ruleForName,
+      description: ruleForDescription,
+    } = requiredField;
+
+    if (ruleForId) {
+      return ruleForId.length !== newId.length;
+    }
+
+    if (ruleForName) {
+      return (
+        newName.length > ruleForName.maxLength &&
+        newName.length < ruleForName.minLength
+      );
+    }
+
+    if (ruleForDescription) {
+      return (
+        newDescription.length > ruleForDescription.maxLength &&
+        newDescription.length < ruleForDescription.minLength
+      );
+    }
+
+    return null;
+  });
+  if (fieldsWithError.length > 0) {
+    throw new NonValidValueError(
+      `New task is invalid, please provide a ${fieldsWithError.toString()}.`,
+    );
   }
 
-  return {
+  const entity = {
     ...newTask,
-    status: "NEW"
-  }
+    acceptanceCritera: null,
+    discussion: null,
+    assignedTo: null,
+    estimatedTime: 0,
+    burnedTime: 0,
+    pendingTime: 0,
+    status: TaskStatus.new,
+    activityType: null,
+    relatedTask: null,
+  };
 
-}
+  db[entity.id] = entity;
 
-createTasks()
+  return entity;
+};
+
+const getAllTasks = () => {
+  return Object.values(db);
+};
+
+export { createTasks, getAllTasks };
